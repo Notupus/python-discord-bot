157a814d0f14c9edb5e38f530e4975a9f868f3fb
Notupus <notpp46@gmail.com>
2020-08-26 17:51:18 +0300

    Added commands Failing

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import os
 import discord
 try:
     import configparser
@@ -6,14 +7,43 @@ except:
 
 config = configparser.ConfigParser()
 config.read('config.ini')
-print ("Logged in as api",(config['api'].get('token')))
+client = discord.Client()
+if config['output'].getboolean('show_bot_identification'):
+
+    print ("Attemping to log in as api",(config['api'].get('token')))
+else:
+    print("Attemping to log in to API")
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+        if config['output'].getboolean('show_bot_identification'):
+         print('Logged on as {0}!'.format(self.user))
+        else:
+            print("Not showing any identifiction in output is disabled. Change the show_bot_identification parameter to true in config.ini to change that ")
 
     async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+       if config['output'].getboolean('show_incoming_messeges'):
+          print('Message from {0.author}: {0.content}'.format(message))
+
+
+    async def on_member_join(member):
+        if config['actions'].getboolean('greet'):
+           await member.create_dm()
+           await member.dm_channel.send(
+         f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+
+    async def on_message(message):
+      if message.author == client.user:
+        return
+
+      if message.content.startswith('$hello'):
+        if config['actions'].getboolean('greet_back'):
+         await message.channel.send('Hello!')
+
+
 
 client = MyClient()
 client.run(config['api'].get('token'))
+
